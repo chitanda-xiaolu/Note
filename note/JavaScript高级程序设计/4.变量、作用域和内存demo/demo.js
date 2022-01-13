3f4ed40e6faa3d8b6afbdc03b046f6dab9c44ac2
chitanda-xiaolu <183138896@qq.com>
2022-01-13 11:12:22 +0800

    update

--- note/JavaScript高级程序设计/4.变量、作用域和内存demo/demo.js
+++ note/JavaScript高级程序设计/4.变量、作用域和内存demo/demo.js
@@ -1,33 +1,33 @@
 /**
  * demo1
  */
-let color = "blue"
-function changeColor() {
-    if (color === "blue") {
-        color = "red"
-    } else {
-        color = "blue"
-    }
-}
-changeColor()
-console.log("Color is now " + color)
+// let color = "blue"
+// function changeColor() {
+//     if (color === "blue") {
+//         color = "red"
+//     } else {
+//         color = "blue"
+//     }
+// }
+// changeColor()
+// console.log("Color is now " + color)
 
 /**
  * demo2
  */
 
-let color = "blue"
-function changeColor() {
-    let anotherColor = "red"
-    function swapColors() {
-        let tempColor = anotherColor
-        anotherColor = color
-        color = tempColor
-        //这里可以访问color、anotherColor和tempColor
-    }
-    //这里可以访问color和anotherColor、但不能访问tempColor
-    swapColors()
-}
+// let color = "blue"
+// function changeColor() {
+//     let anotherColor = "red"
+//     function swapColors() {
+//         let tempColor = anotherColor
+//         anotherColor = color
+//         color = tempColor
+//         //这里可以访问color、anotherColor和tempColor
+//     }
+//     //这里可以访问color和anotherColor、但不能访问tempColor
+//     swapColors()
+// }
 
 //这里只能访问color
 
@@ -37,3 +37,8 @@ function changeColor() {
  * 中的任何变量和函数。所以swapColors()可以自内向外访问changecolor()环境里和全局环境里的
  * 变量。而全局环境不能访问自外访问changecolor()和swapColors()环境中的变量。
  */
+
+let num = [1, 2, 3]
+if (num instanceof Array) {
+    console.log("num is array")
+}
