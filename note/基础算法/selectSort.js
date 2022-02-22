844e7610e8a9ec4251738518e691d71368150bb2
chitanda-xiaolu <183138896@qq.com>
2022-02-22 18:40:12 +0800

    update

--- note/基础算法/selectSort.js
+++ note/基础算法/selectSort.js
@@ -1,19 +1,19 @@
-function selectSort(array) {
-  let length = array.length
+// function selectSort(array) {
+//   let length = array.length
 
-  if(!Array.isArray(array) || length <= 1) {return}
-  for(let i = 0; i < length - 1; i++) {
-    let minIndex = i
-    for(let j = i + 1; j < length; j++) {
-      if(array[minIndex] > array[j]) {
-        minIndex = j
-      }
-    }
-    [array[i], array[minIndex]] = [array[minIndex], array[i]]
-  }
+//   if(!Array.isArray(array) || length <= 1) {return}
+//   for(let i = 0; i < length - 1; i++) {
+//     let minIndex = i
+//     for(let j = i + 1; j < length; j++) {
+//       if(array[minIndex] > array[j]) {
+//         minIndex = j
+//       }
+//     }
+//     [array[i], array[minIndex]] = [array[minIndex], array[i]]
+//   }
 
-  return array
-}
+//   return array
+// }
 
-l = [7, 1, 6, 4, 2, 5, 3]
-console.log(selectSort(l))
+// l = [7, 1, 6, 4, 2, 5, 3]
+// console.log(selectSort(l))
