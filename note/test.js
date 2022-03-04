9e182adc54662ae555ac2c6e46d6c1630b79be63
chitanda-xiaolu <183138896@qq.com>
2022-03-04 17:59:51 +0800

    update

--- note/test.js
+++ note/test.js
@@ -1,15 +1,5 @@
-// function Point(x = 0, y = 0) {
-//   this.x = x;
-//   this.y = y;
-// }
-
-// const p = new Point();
-// console.log(p)
-
-let x = 99;
-function foo(p = x + 1) {
-  console.log(p);
+function foo({x, y = 5} = {}) {
+  console.log(x, y);
 }
 
-foo()
-foo()
+foo({x: 1, y: 2}) // undefined 
