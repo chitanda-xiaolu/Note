e7afd41daa462204e7450033672fce982467dd41
chitanda-xiaolu <53386241+chitanda-xiaolu@users.noreply.github.com>
2022-08-24 16:52:56 +0800

    Update test.js

--- note/test.js
+++ note/test.js
@@ -1,2 +1,23 @@
 
 console.log("\u0061")
+[‎2022/‎8/‎24 16:31] lilong kf9209 (CW): 
+Import-Module posh-git # 引入 posh-git 
+
+Import-Module oh-m...
+Import-Module posh-git # 引入 posh-git
+Import-Module oh-my-posh # 引入 oh-my-posh 1_shell
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_shell.omp.json" | Invoke-Expression
+
+
+Set-PSReadlineKeyHandler -Key Tab -Function Complete # 设置 Tab 键补全
+Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function MenuComplete # 设置 Ctrl+d 为菜单补全和 Intellisense
+Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo # 设置 Ctrl+z 为撤销
+Set-PSReadLineKeyHandler -Key UpArrow -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchBackward()
+    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+    }
+    Set-PSReadLineKeyHandler -Key DownArrow -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchForward()
+    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+    }
+clear
