587557d53e3a2c106d1018bcf088576732f89f86
chitanda-xiaolu <183138896@qq.com>
2022-09-02 00:53:04 +0800

    vue响应式

--- note/开发笔记/vue/vue原理/observe.js
+++ note/开发笔记/vue/vue原理/observe.js
@@ -3,8 +3,22 @@ function updateView() {
   console.log('更新视图')
 }
 
+// 重新定义数组原型
+const oldArrayProperty = Array.prototype
+// 创建新对象，原型指向oldArrayProperty,在扩展新的方法不会影响原型
+const arrProto = Object.create(oldArrayProperty)
+['push', 'pop', 'shift', 'unshift', 'splice'].forEach(methodName => {
+  arrProto[methodName] = function () {
+    updateView() //更新视图
+    oldArrayProperty[methodName].call(this, ...arguments)
+  }
+})
+
 // 重新定义属性，监听起来
 function defineReactive(target, key, value) {
+  //递归监听
+  observer(value)
+
   // 核心API
   Object.defineProperty(target, key, {
     get() {
@@ -12,6 +26,7 @@ function defineReactive(target, key, value) {
     },
     set(newValue) {
       if (newValue !== value) {
+        observer(newValue)
         value = newValue
         //更新视图
         updateView()
@@ -35,19 +50,21 @@ function observer(target) {
 const data = {
   name: 'chitanda',
   age: 20,
-  // info: {
-  //   address: 'hangzhou'
-  // },
-  // nums: [10, 20, 30]
+  info: {
+    address: 'hangzhou'
+  },
+  nums: [10, 20, 30]
 }
 
 
 // 监听数据
 observer(data)
 
-console.log(data.name)
-data.name = "chitanda-xiaolu"
-data.age = 21
-data.x = '100'
-// delete data.name
-console.log(data.name)
+// console.log(data.name)
+// data.name = "chitanda-xiaolu"
+// data.age = 21
+// data.x = '100'
+// data.info.address = 'shanghai'
+// console.log(data.name)
+
+data.nums.push(40)
